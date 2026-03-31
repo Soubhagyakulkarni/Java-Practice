2cac459c11a83cfe189dffe448cf6215ea2f679c
Soubhagya Kulkarni <soubhagyagk9@gmail.com>
2026-03-31 19:28:00 +0530

    Add files via upload

---
+++ Reverse_array.java
@@ -0,0 +1,34 @@
+public class Reverse_array {
+   public static void reverse(int numbers[]){
+      int first=0,last=numbers.length-1;
+      while(first<last)
+      {
+         int temp =numbers[last];
+         numbers[last] = numbers[first];
+         numbers[first] = temp;
+         first++;
+         last--;
+      }
+      
+   }
+   public static void main (String args[]){
+      int numbers[] = {2,4,6,8,10,};
+      reverse(numbers);
+      System.out.println("the reverse of the given array:");
+      for(int i=0;i<numbers.length;i++){
+      System.out.print(" "+numbers[i]);
+   }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+

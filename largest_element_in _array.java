c807a2c65042996c13d3925c969e3fb0c90b845f
Soubhagya Kulkarni <soubhagyagk9@gmail.com>
2026-03-31 19:00:55 +0530

    Add files via upload

---
+++ largest_element_in _array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class largest_element_in_array {
+   public static int grtlargest(int numbers[]) {
+      int largest = Integer.MIN_VALUE;
+      for (int i = 0; i < numbers.length; i++) {
+         if (largest < numbers[i]) {
+            largest = numbers[i];
+         }
+      }
+      return largest;
+   }
+
+   public static void main(String args[]) {
+      int numbers[] = { 1, 2, 3, 4, 5 };
+      System.out.println("The largest value is:" + getlargest(numbers));
+   }
+
+}
